7dd36b9d44633f2a1d1dea3e071447c1d09f8dfe
Piyush Aaryan <piyushaaryan011@gmail.com>
2024-07-24 05:57:08 +0530

    Update file_utils.py

--- fooocusapi/utils/file_utils.py
+++ fooocusapi/utils/file_utils.py
@@ -12,9 +12,9 @@ from PIL.PngImagePlugin import PngInfo
 import firebase_admin
 from firebase_admin import credentials, storage
 import time
-cred = credentials.Certificate("/workspace/Virtual AI Character Firebase Admin.json")
+cred = credentials.Certificate("/workspace/furniture-ai service account.json")
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'virtual-ai-character.appspot.com'
+    'storageBucket': 'furniture-ai-1aeec.appspot.com'
 })
 
 def convert_to_url(local_file, cloud_file):
